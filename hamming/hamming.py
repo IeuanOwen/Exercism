0e992ad7195aa3da216eaf8424f7921747b0fd15
IeuanOwen <ieuanrhysowen@btinternet.com>
2019-09-19 10:56:19 +0100

    Print statements removed, list comprehension implemeted

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,14 +1,4 @@
-
-
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("Strings are not the same length")
-    diff = 0
-    for x, y in zip(strand_a, strand_b):
-        print(x)
-        print(y)
-        if x != y:
-            diff += 1
-    print(diff)
-    return diff
-
+    return sum([1 for x, y in zip(strand_a, strand_b) if x != y])
