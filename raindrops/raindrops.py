1b9f43697d07e0843e6dd2020d6c60edee5a4761
IeuanOwen <ieuanrhysowen@btinternet.com>
2019-10-09 16:07:19 +0100

    Task Complete -code improved

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,13 +1,9 @@
 def convert(number):
-    noFactor = str(number)
     answer = ""
-    if number % 3 != 0 and number % 5 != 0 and number % 7 != 0: #tests if any of the values are a factor
-        answer = noFactor
-    else:
-        if number % 3 == 0:
-            answer = answer + "Pling"
-        if number % 5 == 0:
-            answer = answer + "Plang"
-        if number % 7 == 0:
-            answer = answer + "Plong"
-    return answer
+    if number % 3 == 0:
+        answer = answer + "Pling"
+    if number % 5 == 0:
+        answer = answer + "Plang"
+    if number % 7 == 0:
+        answer = answer + "Plong"
+    return answer if answer else str(number) #returns value of answer variable or the number if answer is empty
