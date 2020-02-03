f269b5a92a81fcf324fad3a97ae42958d00490b9
IeuanOwen <ieuanrhysowen@btinternet.com>
2020-02-03 22:13:01 +0000

    Beer Songs submitted for feedback, best the code has ever been

--- beer-song/beer_song.py
+++ beer-song/beer_song.py
@@ -1,24 +1,38 @@
-def recite(start, take=1):
-    phrase1 = "{} {} of beer on the wall, {} {} of beer."
-    phrase_sec_line1 = "Take {} down and pass it around, {} {} of beer on the wall."
-    phrase2 = "No more bottles of beer on the wall, no more bottles of beer."
-    phrase2_sec_line = "Go to the store and buy some more, 99 bottles of beer on the wall."
 
-# figure out how to loop in reverse throught the range of take
+def first_verse_line(num, plur):
+    """Format string for verse line 1"""
+    num2 = num if num != "No more" else "no more"
+    line = f"{num} {plur} of beer on the wall, {num2} {plur} of beer."
+    return line
+
+def sec_verse_line(num, plural="bottles", var="one"):
+    """Format string for verse line 2"""
+    line = f"Take {var} down and pass it around, {num} {plural} of beer on the wall."
+    return line
+
+def recite(start, take=1):
+    last_line = "Go to the store and buy some more, 99 bottles of beer on the wall."
+    song = []
+
     for i in range(take):
-        start = take
-        if start == 0:
-            print(phrase2)
-            return [phrase2, phrase2_sec_line]
-        elif start == 1:
-            phrase = [phrase1.format(start, "bottle", start, "bottle"), phrase_sec_line1.format("it", "no more", "bottles")]
-            print(phrase)
-            return phrase
-        elif start == 2:
-            phrase = [phrase1.format(start, "bottles", start, "bottles"), phrase_sec_line1.format("one", start - 1, "bottle")]
-            print(phrase)
-            return phrase
+        plural = "bottles" if start != 1 else "bottle"
+
+        if start != 0:
+            song.append(first_verse_line(start, plural))
+            num = start - 1
+            if start > 2:
+                song.append(sec_verse_line(num))
+
+            elif start == 2:
+                song.append(sec_verse_line(num, "bottle"))
+            else:
+                song.append(sec_verse_line("no more", var="it"))
+            start -= 1
         else:
-            phrase = [phrase1.format(start, "bottles", start, "bottles"), phrase_sec_line1.format("one", start - 1, "bottles")]
-            print(phrase)
-            return phrase
+            song.append(first_verse_line("No more", plural))
+            song.append(last_line)
+
+        if start != 0 and i != take-1:
+            song.append("")
+
+    return song
