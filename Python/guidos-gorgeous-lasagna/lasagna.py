f701c87fad635e1d212428424284ea6045f00e20
IeuanOwen <ieuanrhysowen@btinternet.com>
2022-08-04 10:49:08 +0100

    Task Completed

--- Python/guidos-gorgeous-lasagna/lasagna.py
+++ Python/guidos-gorgeous-lasagna/lasagna.py
@@ -3,13 +3,12 @@
 Learn about Guido, the creator of the Python language: https://en.wikipedia.org/wiki/Guido_van_Rossum
 """
 
-# TODO: define the 'EXPECTED_BAKE_TIME' constant
-# TODO: consider defining the 'PREPARATION_TIME' constant
-#       equal to the time it takes to prepare a single layer
+EXPECTED_BAKE_TIME = 40
+PREPARATION_TIME = 2
 
 
 # TODO: define the 'bake_time_remaining()' function
-def bake_time_remaining():
+def bake_time_remaining(elapsed_bake_time):
     """Calculate the bake time remaining.
 
     :param elapsed_bake_time: int - baking time already elapsed.
@@ -19,12 +18,36 @@ def bake_time_remaining():
     an argument and returns how many minutes the lasagna still needs to bake
     based on the `EXPECTED_BAKE_TIME`.
     """
+    if elapsed_bake_time > 40:
+        return "Your lasagna has burned"
+    elif elapsed_bake_time == EXPECTED_BAKE_TIME:
+        return "Lasagna Ready!!!"
+    return EXPECTED_BAKE_TIME - elapsed_bake_time
 
-    pass
+def preparation_time_in_minutes(layers):
+    """
+    :param layers: int - number of layers your lasagna should have.
+    :return: int - time taken to create lasagna in mins derived from 'PREPARATION_TIME'.
+
+    Function that takes the number of layers you want to add to your Lasagna
+    as an argument and returns how many minuetes the lasagna will take to prepare.
+    """
+    return PREPARATION_TIME * layers
 
 
-# TODO: define the 'preparation_time_in_minutes()' function
-#       and consider using 'PREPARATION_TIME' here
+def elapsed_time_in_minutes(layers, elapsed_bake_time):
+    """
+    :param layers: int - number of layers your lasagna should has.
+    :param elapsed_bake_time: int - baking time already elapsed.
+    :return: int - time spent creating lasagna in mins so far.
+
+    Function that takes the number of layers you want to add to your lasagna
+    and how many minuets it has been cooking so far as arguments and returns how
+    many minuetes you have spent preparing the lasagna so far.
+    """
+    prep_time = preparation_time_in_minutes(layers)
+    return elapsed_bake_time + prep_time
+
 
 
 # TODO: define the 'elapsed_time_in_minutes()' function
