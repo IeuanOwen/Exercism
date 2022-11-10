3a7a246f5449ada091f2ddd090f9b77fa235e78b
IeuanOwen <ieuanrhysowen@btinternet.com>
2022-11-10 22:02:19 +0000

    Task Completed

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,14 @@
+// Package weather forcasts current weather condtions for Goblinocus.
 package weather
 
+// CurrentCondition represents the current weather condition.
 var CurrentCondition string
+
+// CurrentLocation represents a certain city in Goblinocus.
 var CurrentLocation string
 
+// Forecast returns a string value that explains the current Weather
+// in a certain location in Goblinocus.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
